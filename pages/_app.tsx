aa688d510f960840ffd49ec22932b414527a8a2d
David Granado <davidjgranado@gmail.com>
2022-04-23 11:57:15 -0500

    add _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,33 @@
-import '../styles/globals.css';
+import Head from 'next/head';
+
 import type { AppProps } from 'next/app';
 
-function MyApp({
-	Component, pageProps,
-}: AppProps) {
-	return <Component {...pageProps} />;
+import '@styles/globals.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+interface Props {
+	initialState: any;
+}
+
+function MyApp(props: AppProps<Props>) {
+	const {
+		Component,
+		pageProps,
+	} = props;
+
+	return (
+		<>
+			<Head>
+				<meta
+					name="viewport"
+					content="initial-scale=1, width=device-width"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>);
 }
 
 export default MyApp;
