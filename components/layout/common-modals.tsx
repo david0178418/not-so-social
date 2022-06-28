735d62e8319cb0065c2af9729ec03c406211b55c
David Granado <davidjgranado@gmail.com>
2022-06-27 23:06:01 -0500

    remove conditional login/out modal

--- components/layout/common-modals.tsx
+++ components/layout/common-modals.tsx
@@ -1,19 +1,14 @@
 import { LoginModal } from '@components/modals/login.modal';
 import { LogoutModal } from '@components/modals/logout.modal';
 import { CreatePostModal } from '@components/modals/create-post.modal';
-import { useIsLoggedIn } from '@common/hooks';
 import { BoostPostModal } from '@components/modals/boost-post.modal';
 
 export
 function CommonModals() {
-	const isLoggedIn = useIsLoggedIn();
 	return (
 		<>
-			{isLoggedIn ? (
-				<LogoutModal />
-			) : (
-				<LoginModal />
-			)}
+			<LogoutModal />
+			<LoginModal />
 			<CreatePostModal />
 			<BoostPostModal />
 		</>
