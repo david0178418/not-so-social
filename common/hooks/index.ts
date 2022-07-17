f996008008bca10fab9d6ce1810977272d04c127
David Granado <davidjgranado@gmail.com>
2022-07-17 15:43:42 -0500

    Move hooks into directory

--- common/hooks/index.ts
+++ common/hooks/index.ts
@@ -1,7 +1,7 @@
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import { UserRoles } from './constants';
+import { UserRoles } from '@common/constants';
 
 export
 function useIsLoggedIn() {
