4436c1d257df7abddfd5195f4e957b1a5db59e1e
David Granado <davidjgranado@gmail.com>
2022-07-27 15:38:39 -0500

    Switch from setTimeout usage to useTimeout

--- components/layout/common-stuff.tsx
+++ components/layout/common-stuff.tsx
@@ -4,31 +4,27 @@ import { CreatePostModal } from '@components/modals/create-post.modal';
 import { BoostPostModal } from '@components/modals/boost-post.modal';
 import { Loader } from '@components/loader';
 import { Toast } from '@components/toast';
-import { useEffect } from 'react';
 import { getNotificaitons } from '@common/client/api-calls';
 import { pushToastMsgAtom } from '@common/atoms';
 import { useSetAtom } from 'jotai';
-import { useIsLoggedIn } from '@common/hooks';
+import { useIsLoggedIn, useTimeout } from '@common/hooks';
 
 export
 function CommonStuff() {
 	const pushToastMsg = useSetAtom(pushToastMsgAtom);
 	const isLoggedIn = useIsLoggedIn();
 
-	useEffect(() => {
+	useTimeout(() => {
 		if(!isLoggedIn) {
 			return;
 		}
 
-		const reset = setTimeout(() => {
-			// TODO where should this logic go?
-			getNotificaitons()
-				.then(notifications => {
-					notifications.map(n => pushToastMsg({ message: n.message }));
-				});
-		}, 1000);
-		return () => clearTimeout(reset);
-	}, [isLoggedIn]);
+		// TODO where should this logic go?
+		getNotificaitons()
+			.then(notifications => {
+				notifications.map(n => pushToastMsg({ message: n.message }));
+			});
+	}, 1000);
 
 	return (
 		<>
