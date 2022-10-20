869e06a43cfc35fa358f5011220deda18c4fdd39
David Granado <davidjgranado@gmail.com>
2022-10-20 13:04:59 -0500

    Add another image

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -1,12 +1,13 @@
 import { NextSeo } from 'next-seo';
 import NotSoSocialImg from '@components/layout/left-rail/NotSo.Social.png';
+import NotSoSocialStackedImg from '@components/layout/left-rail/NotSo.Social-stacked.png';
 import NotSoSocialMedImg from '@components/layout/left-rail/NotSo.Social-med.png';
 import NotSoSocialMedJpegImg from '@components/layout/left-rail/NotSo.Social-med.jpg';
+import { urlJoin } from '@common/utils';
 import {
 	AppName,
 	BaseUrl,
 } from '@common/constants';
-import { urlJoin } from '@common/utils';
 
 interface Props {
 	title?: string;
@@ -15,6 +16,7 @@ interface Props {
 }
 
 const NotSoSocialImgSrc = urlJoin(BaseUrl, NotSoSocialImg.src);
+const NotSoSocialStackedImgSrc = urlJoin(BaseUrl, NotSoSocialImg.src);
 const NotSoSocialMedImgSrc = urlJoin(BaseUrl, NotSoSocialMedImg.src);
 const NotSoSocialMedJpegImgSrc = urlJoin(BaseUrl, NotSoSocialMedJpegImg.src);
 
@@ -40,15 +42,15 @@ function Seo(props: Props) {
 					title,
 					site_name: AppName,
 					images: [ {
-						url: NotSoSocialMedJpegImgSrc,
-						width: NotSoSocialMedJpegImg.width,
-						height: NotSoSocialMedJpegImg.height,
-						alt: 'NoSo.Social',
-						type: 'image/jpeg',
-					}, {
 						url: NotSoSocialImgSrc,
 						width: NotSoSocialImg.width,
 						height: NotSoSocialImg.height,
+						alt: 'NoSo.Social',
+						type: 'image/png',
+					}, {
+						url: NotSoSocialStackedImgSrc,
+						width: NotSoSocialStackedImg.width,
+						height: NotSoSocialStackedImg.height,
 						alt: 'NoSo.Social',
 						type: 'image/png',
 					}, {
@@ -57,6 +59,12 @@ function Seo(props: Props) {
 						height: NotSoSocialMedImg.height,
 						alt: 'NoSo.Social',
 						type: 'image/png',
+					}, {
+						url: NotSoSocialMedJpegImgSrc,
+						width: NotSoSocialMedJpegImg.width,
+						height: NotSoSocialMedJpegImg.height,
+						alt: 'NoSo.Social',
+						type: 'image/jpeg',
 					}],
 				}}
 			/>
